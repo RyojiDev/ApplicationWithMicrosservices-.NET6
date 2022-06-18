85eb1b5d402e494a412507776f9d05d3bd6b6352
Ryoji Kitano Neto <ryojineto@employer.com.br>
2022-06-17 23:51:05 -0300

    feature - Created DbContext and include deppendencies

--- GeekShopping.ProductApi/Program.cs
+++ GeekShopping.ProductApi/Program.cs
@@ -1,9 +1,23 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("MySqlConnectionString");
+
+/* anotherForm
+ * var connection = builder.Configuration["connectionString:MySqlConnectionString"];*/
+
+builder.Services
+            .AddDbContext<MySqlContext>(options => options
+            .UseMySql(connection, 
+                new MySqlServerVersion(
+                    new Version(8,0,15))));
 
 var app = builder.Build();
 
