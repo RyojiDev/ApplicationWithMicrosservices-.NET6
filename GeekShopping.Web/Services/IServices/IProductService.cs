06248ecfadb05bab535a77d4d6665a7e2ccc6862
RyojikitanoDev <ryojikitanodev@gmail.com>
2022-06-20 23:35:00 -0300

    add - init implement of Web application and HttpClient to make requests

---
+++ GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProductModels();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProdcut(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+
+    }
+}
